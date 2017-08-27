773f10b12c0e84d886126b925d258b1c10a45ce3
PETYAggg <petyaggg@gmail.com>
2017-08-27 21:30:54 +0300

    first commit

---
+++ src/input_numbers/FloatNumTest.java
@@ -0,0 +1,14 @@
+package input_numbers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FloatNumTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
