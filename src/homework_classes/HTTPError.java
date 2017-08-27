7523c0d5adc5f528e68ebb86f11bf2182366ca67
PETYAggg <petyaggg@gmail.com>
2017-08-28 02:45:39 +0300

    fixed format

--- src/homework_classes/HTTPError.java
+++ src/homework_classes/HTTPError.java
@@ -4,45 +4,40 @@ import java.util.Scanner;
 
 public enum HTTPError {
 
-	HTTP400(400, "Bad Request"), HTTP401(401, "Unauthorized"), HTTP402(402, "Payment Required"), HTTP403(403,
-			"Forbidden"), HTTP404(404, "Not Found"), HTTP405(405, "Method Not Allowed"), HTTP406(406,
-					"Not Acceptable"), HTTP407(407, "Proxy Authentication Required"), HTTP408(408,
-							"Request Timeout"), HTTP409(409, "Conflict"), HTTP410(410, "Gone"), HTTP411(411,
-									"Length Required"), HTTP412(412, "Precondition Failed"), HTTP413(413,
-											"Payload Too Large"), HTTP414(414, "Request-URI Too Long"), HTTP415(415,
-													"Unsupported Media Type"), HTTP416(416,
-															"Requested Range Not Satisfiable"), HTTP417(417,
-																	"Expectation Failed"), HTTP418(418,
-																			"I'm a teapot"), HTTP419(419,
-																					"Authentication Timeout"), HTTP420(
-																							420,
-																							"Method Failure"), HTTP422(
-																									422,
-																									"Unprocessable Entity"), HTTP423(
-																											423,
-																											"Locked"), HTTP424(
-																													424,
-																													"Failed Dependency"), HTTP426(
-																															426,
-																															"Upgrade Required"), HTTP428(
-																																	428,
-																																	"Precondition Required"), HTTP429(
-																																			429,
-																																			"Too Many Requests"), HTTP431(
-																																					431,
-																																					"Request Header Fields Too Large"), HTTP440(
-																																							440,
-																																							"Login Timeout"), HTTP444(
-																																									444,
-																																									"No Response"), HTTP449(
-																																											449,
-																																											"Retry With"), HTTP450(
-																																													450,
-																																													"Blocked by Windows Parental Controls"), HTTP451(
-																																															451,
-																																															"Unavailable For Legal Reasons"), HTTPnone(
-																																																	0,
-																																																	"No such error in db");
+	HTTP400(400, "Bad Request"), 
+	HTTP401(401, "Unauthorized"), 
+	HTTP402(402, "Payment Required"), 
+	HTTP403(403, "Forbidden"), 
+	HTTP404(404, "Not Found"), 
+	HTTP405(405, "Method Not Allowed"),
+	HTTP406(406, "Not Acceptable"),
+	HTTP407(407, "Proxy Authentication Required"), 
+	HTTP408(408, "Request Timeout"), 
+	HTTP409(409, "Conflict"), 
+	HTTP410(410, "Gone"), 
+	HTTP411(411,"Length Required"),
+	HTTP412(412, "Precondition Failed"), 
+	HTTP413(413, "Payload Too Large"), 
+	HTTP414(414, "Request-URI Too Long"), 
+	HTTP415(415, "Unsupported Media Type"), 
+	HTTP416(416, "Requested Range Not Satisfiable"), 
+	HTTP417(417, "Expectation Failed"), 
+	HTTP418(418, "I'm a teapot"),
+	HTTP419(419,"Authentication Timeout"),
+	HTTP420(420,"Method Failure"), 
+	HTTP422(422,"Unprocessable Entity"),
+	HTTP423(423, "Locked"), 
+	HTTP424(424,"Failed Dependency"), 
+	HTTP426(426,"Upgrade Required"),
+	HTTP428(428, "Precondition Required"),
+	HTTP429(429, "Too Many Requests"),
+	HTTP431(431,"Request Header Fields Too Large"), 
+	HTTP440(440,"Login Timeout"), 
+	HTTP444(444, "No Response"),
+	HTTP449(449, "Retry With"), 
+	HTTP450(450, "Blocked by Windows Parental Controls"), 
+	HTTP451(451,"Unavailable For Legal Reasons"), 
+	HTTPnone(0,"No such error in db");
 	int code;
 	String message;
 
