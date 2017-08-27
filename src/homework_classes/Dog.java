4c2fbcede05e9ac83c349a4433a469a0a9f25eb0
PETYAggg <petyaggg@gmail.com>
2017-08-27 22:00:43 +0300

    Renamed folder

---
+++ src/homework_classes/Dog.java
@@ -0,0 +1,79 @@
+package homework_classes;
+
+import java.util.Set;
+
+
+ class Dog {
+	
+	String name;
+	Integer age;
+	breed br;
+	
+	public enum breed {
+		taksa,
+		haski,
+		vivcharka;
+	}
+	
+	
+	
+	public Dog() {
+	
+		name=null;
+		age=0;
+		
+	}
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+
+	public Integer getAge() {
+		return age;
+	}
+
+
+
+	public breed getBr() {
+		return br;
+	}
+
+
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == null) return false;
+	    if (other == this) return true;
+	    if (!(other instanceof Dog))return false;
+	    Dog otherMyClass = (Dog)other;
+	    if(otherMyClass.name==this.name ){
+	    	return true;
+	    }
+	    else {
+	    	return false;
+	    }
+	}
+	
+	@Override
+	 public int hashCode() {
+	      return 12324;
+	   }
+	
+	@Override
+	public String toString() {
+		return "Name: "+name+" Breed: "+br+" Age: "+age;
+	}
+	
+
+	public static void main(String[] args) {
+		
+	
+	
+	
+	}
+}
+
---
+++ src/homework_classes/DogCollection.java
@@ -0,0 +1,66 @@
+package homework_classes;
+
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import homework_classes.Dog.breed;
+
+public class DogCollection {
+	
+	
+	public Set<Dog> Dogs;
+	
+	public DogCollection() {
+		
+		Dogs = new HashSet<Dog>() ;
+	}
+	
+	public void AddDog(String name, breed b, Integer age) {
+		Dog doge=new Dog();
+		if(name!=null & age>0){
+			doge.name=name;
+			doge.br=b;
+			doge.age=age;
+			Dogs.add(doge);
+		}
+		
+		else {
+			System.out.println("Bad input");
+		}
+			
+	}
+	
+	public void GetTheOldest() {
+		Dog doge = new Dog();
+		
+		for(Dog d:Dogs){
+			if(d.age>doge.age){
+				doge=d;
+			}
+		}
+		if(doge.name!=null) {
+			System.out.println("The oldest dog is: "+doge.getName()+". It's breed is: "+doge.getBr()  );
+		}
+		else {
+			System.out.println("No dogs was found");
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		DogCollection d= new DogCollection();
+		
+		d.AddDog("Brovko",breed.haski,5);
+		d.AddDog("HotDog",breed.taksa,3);
+		d.AddDog("Gavkalo",breed.vivcharka,4);
+		d.AddDog("Brovko",breed.haski,2);
+		
+		System.out.println(d.Dogs.toString());
+		d.GetTheOldest();
+	}
+
+}
