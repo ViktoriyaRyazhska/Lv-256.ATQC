1b30e7d1f2367f5d934ab4e4d45c2962e6099cd3
BohdanZhyvko <bohdan.zhyvko@gmail.com>
2017-08-28 20:59:34 +0300

    Added class 'Dog' with constructor and enum 'Breed' and geters and seters

---
+++ src/edu/com/junit/homework/Dog.java
@@ -0,0 +1,58 @@
+package edu.com.junit.homework;
+
+/**
+ * 
+ * @version 1.0
+ * @author Bohdan Zhyvko
+ */
+class Dog {
+	private String name;
+	private Breed breed;
+	int age;
+
+	public Dog() {
+	}
+
+	public Dog(String name, Breed breed, int age) {
+		super();
+		this.name = name;
+		this.breed = breed;
+		this.age = age;
+	}
+	
+	public enum Breed {
+		Cocker_Spaniel,
+		Chow_Chow,
+		Boxer;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Breed getBreed() {
+		return breed;
+	}
+
+	public void setBreed(Breed breed) {
+		this.breed = breed;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + ", breed=" + breed + ", age=" + age + "]";
+	}
+}
