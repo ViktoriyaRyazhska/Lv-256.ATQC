a3470c1b269a32965d8042a14080a8dd12f4ffcd
Юрій <@Cinder>
2017-08-30 15:22:32 +0300

    Working app

--- src/edu/com/homework/main/Main.java
+++ src/edu/com/homework/main/Main.java
@@ -2,49 +2,68 @@ package edu.com.homework.main;
 
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Scanner;
+import edu.com.homework.tasksbook.Task330;
+import edu.com.homework.tasksbook.Task86a;
+import edu.com.homework.tasksbook.Task86b;
 
 public class Main {
 
-	public static void main(String[] args) {
-		//Does not work (((
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 			Scanner scan = new Scanner(System.in);
 			System.out.print("Enter method number: ");
 			String methodName = scan.next();
-			Methods m = Methods.mNull;
-			m = m.setValue(methodName);
-			Object clazz = m.getClazz();
-			Class cl = clazz.getClass();
-			System.out.println(clazz);
-			String met="";
-			try {
-				Method method= cl.getMethod(m.getMethodName(), null);
-				try {
-					 met = (String) method.invoke(clazz, null);
-				} catch (IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} catch (NoSuchMethodException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println(met);
-			
-			
-			
-			
+			Object obj = setValue(methodName);
+			obj.getClass().getMethod("resolve", null).invoke(obj, null);
 			
 	}
+	public static Object setValue(String value) {
+		switch (value) {
+		case "86a":
+			return new Task86a();
+		case "86b":
+			return new Task86b();
+		case "330":
+			return new Task330();
+		case "87":
+			return null;
+		case "226":
+			return null;
+		case "559":
+			return null;
+		case "88a":
+			return null;
+		case "88b":
+			return null;
+		case "322":
+			return null;
+		case "88w":
+			return null;
+		case "88g":
+			return null;
+		case "332":
+			return null;
+		case "107":
+			return null;
+		case "243a":
+			return null;
+		case "243b":
+			return null;
+		case "108":
+			return null;
+		case "331a":
+			return null;
+		case "331b":
+			return null;
+		case "178b":
+			return null;
+		case "178w":
+			return null;
+		case "554":
+			return null;
+		default:
+			return null;
+		}
 
+}
 }
