eee2cd12b490ebbffb25607b95f3eecb79a93ada
“Vasia <vpupkin@mail.com>
2017-08-31 09:42:09 +0300

    vvv

--- test/homework_classes/DogCollectionTest.java
+++ test/homework_classes/DogCollectionTest.java
@@ -10,51 +10,49 @@ import org.junit.Test;
 
 import homework_classes.Dog.breed;
 
-
 public class DogCollectionTest {
 
 	@Test
-	public void TestAddDog(){
+	public void TestAddDog() {
 		DogCollection dog = new DogCollection();
 		dog.AddDog("brovko", breed.vivcharka, 3);
-		
-		Set<Dog> Doges=new HashSet<Dog>() ;
-		Dog d =new Dog();
-		d.name="brovko";
-		d.br=breed.vivcharka;
-		d.age=3;
+
+		Set<Dog> Doges = new HashSet<Dog>();
+		Dog d = new Dog();
+		d.name = "brovko";
+		d.br = breed.vivcharka;
+		d.age = 3;
 		Doges.add(d);
-		
-		
+
 		Assert.assertEquals(Doges, dog.Dogs);
-		
+
 	}
-	
+
 	@Test
-	public void TestGetTheOldest(){
+	public void TestGetTheOldest() {
 		DogCollection dog = new DogCollection();
-		Dog d =new Dog();
-		d.name="brovko";
-		d.br=breed.vivcharka;
-		d.age=3;
+		Dog d = new Dog();
+		d.name = "brovko";
+		d.br = breed.vivcharka;
+		d.age = 3;
 		dog.Dogs.add(d);
 
-		Dog d1 =new Dog();
-		d.name="brovko_oldest";
-		d.br=breed.vivcharka;
-		d.age=4;
+		Dog d1 = new Dog();
+		d.name = "brovko_oldest";
+		d.br = breed.vivcharka;
+		d.age = 4;
 		dog.Dogs.add(d1);
-		
+
 		Assert.assertEquals("The oldest dog is: brovko_oldest. It's breed is: vivcharka", dog.GetTheOldest());
 	}
-	
+
 	@Test
-	public void TestNoDuplicates(){
+	public void TestNoDuplicates() {
 		DogCollection dog = new DogCollection();
 		dog.AddDog("brovko", breed.vivcharka, 3);
 		dog.AddDog("brovko", breed.vivcharka, 4);
-		
+
 		Assert.assertEquals(1, dog.Dogs.size());
 	}
-	
+
 }
