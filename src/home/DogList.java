22188bfe3d12b7c0ed630e7acb2e04f2d2a96d3b
XTreme.ws <XTreme.ws@192.168.118.109>
2017-08-31 10:00:08 +0300

    o

--- src/home/DogList.java
+++ src/home/DogList.java
@@ -1,7 +1,7 @@
 package home;
 
-import java.util.ArrayList;+
-import java.util.List;+ +
+import java.util.ArrayList;
+import java.util.List;
 
 public class DogList {
 	
---
+++ src/taskbook/task88w.java
@@ -0,0 +1,45 @@
+package taskbook;
+
+
+
+	import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	import java.util.Arrays;
+
+	public class task88w {
+		public String replaceFirstLast(int n) {
+			String s1 = Integer.toString(n);
+			String[] arr = s1.split("");
+
+			String tmp = arr[0];
+			arr[0] = arr[arr.length - 1];
+			arr[arr.length - 1] = tmp;
+
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i]);
+			}
+			String s = Arrays.toString(arr);
+			return s;
+		}
+
+		public void resolve() {
+			System.out.println("Replace first and last digit of number");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter natural number:");
+			int n = 0;
+			try {
+				n = Integer.parseInt(br.readLine());
+			} catch (NumberFormatException e) {
+				System.err.println("You can enter only integer numbers");
+			} catch (IOException e) {
+				System.err.println("IO Exception");
+			}
+			System.out.println(replaceFirstLast(n));
+		}
+	
+	public static void main (String[] args) {
+		task88w Task88w = new task88w();
+		Task88w.resolve();
+}
+}
