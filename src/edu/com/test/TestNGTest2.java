39cd30aeb45f6db172853282d052ee228ce5b50e
Viktoriya <r_va@ukr.net>
2017-09-04 20:54:51 +0300

    add tests with locators

--- src/edu/com/test/TestNGTest2.java
+++
@@ -1,9 +0,0 @@
-package edu.com.test;
-import org.testng.annotations.*;
-
-public class TestNGTest2 {
-	@Test(expectedExceptions = ArithmeticException.class)
-	public void divisionWithException() {
-		int i = 1 / 0;
-	}
-}
--- src/edu/com/test/TestNGTest3.java
+++
@@ -1,9 +0,0 @@
-package edu.com.test;
-import org.testng.annotations.*;
-
-public class TestNGTest3 {
-	@Test(enabled = false)
-	public void divisionWithException() {
-		System.out.println("Method is not ready yet");
-	}
-}
