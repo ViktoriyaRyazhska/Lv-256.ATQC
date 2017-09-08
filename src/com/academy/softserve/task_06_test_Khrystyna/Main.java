00481f067402797d6f29dd63bb308d4150a038e1
Maksym Mandrykovskyy <mandrykovskiy@gmail.com>
2017-09-08 13:05:08 +0300

    add

--- src/com/academy/softserve/task_06_test_Khrystyna/Main.java
+++ src/com/academy/softserve/task_06_test_Khrystyna/Main.java
@@ -2,9 +2,9 @@ package com.academy.softserve.task_06_test_Khrystyna;
 
 public class Main {
     public static void main(String[] args) {
-        Task178b task178b=new Task178b();
-        task178b.resolve();
-        Task178w task178w=new Task178w();
-        task178w.resolve();
+//        Task178b task178b=new Task178b();
+//        task178b.resolve();
+//        Task178w task178w=new Task178w();
+//        task178w.resolve();
     }
 }
