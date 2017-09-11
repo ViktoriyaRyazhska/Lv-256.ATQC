5f2f2d47ff4e53fa81cc36c7e8b320782a638d01
BohdanZhyvko <bohdan.zhyvko@gmail.com>
2017-09-11 11:58:41 +0300

    changed wep site

--- test/com/example/selenium/ide/tests/ExchangeRateUsd.java
+++ test/com/example/selenium/ide/tests/ExchangeRateUsd.java
@@ -15,44 +15,49 @@ public class ExchangeRateUsd {
 	public void setUp() {
 		// initial property
 		System.setProperty("webdriver.gecko.driver", "resources//geckodriver.exe");// "D:\\1\\drivers\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-		// go to kurs.com.ua
-		driver.get("https://kurs.com.ua/");
+		// go to "UBR" web page Exchange rate in Ukraine Banks
+		driver.get("http://ubr.ua/banks");
 	}
 
 	/**
-	 * Test Exchange rate USD is present on page 'Exchange rate in Ukraine'
-	 * kurs.com.ua and print this rate on console.
+	 * Test Exchange rate USD is present on page 'Exchange rate in Ukraine Banks'
+	 * on 'UBR' page and print this rate on console.
 	 */
 	@Test
 	public void testExchangeRateUSD() {
 
-		// check is element present on page
-		assertNotNull("//div[@id='elBoardSummary']//a[contains(text(),'USD')]");
-		//print exchange rate
+		// check is element 'USD' present on page
+		assertNotNull("//table[@id='js-tb-currency']//span[contains(text(), 'USD')]");
+
+		// print exchange rate
 		printExchangeRateUSD();
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		// close WebDriver
 		driver.quit();
 	}
 
-	//print Exchange Rate USD on console
+	/*
+	 * print Exchange Rate USD on console
+	 */
 	private void printExchangeRateUSD() {
-		// get selling rate
-		String selling = driver
-				.findElement(By
-						.xpath("//div[@id='elBoardSummary']//a[contains(text(),'USD')]/following::td[3]/span[@class='ipsKurs_rate']"))
-				.getText();
 		// get buying course
 		String buying = driver
 				.findElement(By
-						.xpath("//div[@id='elBoardSummary']//a[contains(text(),'USD')]/following::td[1]/span[@class='ipsKurs_rate']"))
-				.getText();
+					.xpath("(//table[@id='js-tb-currency']//span[contains(text(), 'USD')]/following::strong)[1]"))
+						.getText();
+
+		// get selling rate
+		String selling = driver
+				.findElement(By
+						.xpath("(//table[@id='js-tb-currency']//span[contains(text(), 'USD')]/following::strong)[2]"))
+							.getText();
+
 		// print exchange rate of USD on console
 		System.out.println("Exchange rate USD: \n" + "Buying: " + buying + ", Selling: " + selling);
 	}
