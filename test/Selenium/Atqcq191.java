3f550d76c24805a582e45a75abd7d6b14ae72350
XTreme.ws <XTreme.ws@DESKTOP-F3FS9N9>
2017-09-13 09:52:47 +0300

    add verifying of color

--- test/Selenium/Atqcq191.java
+++ test/Selenium/Atqcq191.java
@@ -1,13 +1,20 @@
 package Selenium;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 /**
  * This test case verify that Commissioner see co-owners from his community only
@@ -31,38 +38,22 @@ public class Atqcq191 {
 	public void setUp() throws Exception {
 		// driver sets up
 		System.setProperty("webdriver.gecko.driver", "D:\\QA\\ATQC\\selenium drivers\\geckodriver.exe");
-		// System.setProperty("webdriver.gecko.driver", "D:\\1\\drivers\\geckodriver.exe");
+		// System.setProperty("webdriver.gecko.driver",
+		// "D:\\1\\drivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		// login:
 		driver.get("http://regres.herokuapp.com/" + "/login?logout");
 		driver.findElement(By.id("login")).clear();
-		driver.findElement(By.id("login")).sendKeys("andriy");
+		driver.findElement(By.id("login")).sendKeys("admin");
 		driver.findElement(By.id("password")).clear();
-		driver.findElement(By.id("password")).sendKeys("andriy");
+		driver.findElement(By.id("password")).sendKeys("admin");
 		driver.findElement(By.cssSelector("button.btn.btn-primary")).click();// Sign in button
 		// open Active user list:
 		driver.findElement(By.xpath("//a[@href=\"#\" and @data-toggle=\"dropdown\"]")).click();// 'Community' item
 		driver.findElement(By.partialLinkText("Активні")).click();
 	}
 
-	/**
-	 * This method counts rows of table Active co-owners
-	 */
-	public int countTableRows = 0;
-
-	public int countTableRows() throws Exception {
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("edit")));
-		List<WebElement> tableRows = driver.findElements(By.xpath("//tbody/tr"));
-		for (WebElement tableRow : tableRows) {
-			countTableRows++;
-		}
-		return countTableRows;
-	}
-
-	// List<WebElement> tablePages =
-	// driver.findElements(By.cssSelector(".paginate_button"));
-	// for (WebElement tablePage : tablePages) {
 	/**
 	 * This method find Commissioner community
 	 */
@@ -73,17 +64,26 @@ public class Atqcq191 {
 		driver.findElement(By.id("inputIndex3")).clear();
 		driver.findElement(By.id("inputIndex3")).sendKeys(login);
 		driver.findElement(By.id("bth-search")).click();// Search button
-		String commisionerCommnuityName = driver.findElement(By.className("territorialCommunity_name")).getText();
+		// String commisionerCommnuityName = driver.findElement(By.className("territorialCommunity_name")).getText();
+		String commisionerCommnuityName = driver.findElement(By.xpath(".//*[@id='example']/tbody/tr[1]/td[5]"))
+				.getText();
 
 		// Refresh Active co-owners table
 		driver.findElement(By.id("inputIndex3")).clear();
 		driver.findElement(By.id("bth-search")).click();
-
-		// Verify every row with commissioner community name
-		for (int row = 0; row < countTableRows - 1; row++) {
-			assertEquals(driver.findElement(By.className("territorialCommunity_name")).getText(),
-					commisionerCommnuityName);// community name of all users should have the same name with community
-												// of commissioner
+		
+		// for every page of table
+		List<WebElement> tablePages = driver.findElements(By.cssSelector(".paginate_button"));
+		for (WebElement tablePage : tablePages) {
+			// Verify every row with commissioner community name
+			new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("edit")));
+			List<WebElement> tableRows = driver.findElements(By.xpath("//tbody/tr"));
+			for (WebElement tableRow : tableRows) {
+				// assertTrue(true,tableRow.findElements(By.className("territorialCommunity_name")).contains(commisionerCommnuityName));
+				assertEquals(tableRow.findElement(By.className("territorialCommunity_name")).getText(),
+						commisionerCommnuityName);// community name of all users should have the same name with
+													// community of commissioner
+			}
 		}
 	}
 
--- test/Selenium/ErrorMessagesCreatingCoowner.java
+++ test/Selenium/ErrorMessagesCreatingCoowner.java
@@ -1,52 +1,36 @@
 package Selenium;
 
-import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
 
-import org.testng.Assert;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-/**
- * This TC verifies possibility of administrator register new co-owner('Register
- * new user' button) after entering correct data.
- * 
- * @author Kryvenko Yaroslava
- *
- */
+public class ErrorMessagesCreatingCoowner {
 
-public class CreateCoowner {
 	private WebDriver driver;
-	@DataProvider
-	public Object[][] mainRegistrationCorrectData() {
-		return new Object[][] { { "yara", "yara", "111@111.com", "33333333333333333333", "111111", "111111", "Італія" },
-//			{ "yara", "yara", "111@111.com", "444444", "qwerty", "qwerty", "Україна" }
-		};
-	}
-	
-	@DataProvider
-	public Object[][] mainRegistrationInvalidData() {
-		return new Object[][] { { "1111", "1111", "1111", "3333333333333333333333", "111111", "11111", "Італія" },
-//			{ "//@#", "^$&*", "aa@aa", "admin", "qwerty", "qwerty", "Україна" }
-		};
-	}
-	
+
 	/**
 	 * In this method author sets up driver and logging in
 	 */
 	@BeforeClass(alwaysRun = true)
 	public void setUp() throws Exception {
 		System.setProperty("webdriver.gecko.driver", "D:\\QA\\ATQC\\selenium drivers\\geckodriver.exe");
-		// System.setProperty("webdriver.gecko.driver", "D:\\1\\drivers\\geckodriver.exe");
+		// System.setProperty("webdriver.gecko.driver",
+		// "D:\\1\\drivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		// login:
@@ -58,12 +42,27 @@ public class CreateCoowner {
 		driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
 	}
 
-	/**
-	 * In this method author create co-owner
-	 */
-	@Test(dataProvider = "mainRegistrationCorrectData")
-	public void testCreateCowner(String first_Name, String last_Name, String email, String login, String password,
-			String confirm_Password, String territorialCommunity) {
+	@DataProvider
+	public Object[][] registrationInvalidData() {
+		return new Object[][] { { "1111", "1111", "1111", "$%#@", "11", "11111",
+				"Ukraine", "Некоректне введення, лише літери", "Некоректне введення, лише літери",
+				"Введіть коректну адресу",
+				"Логін повинен складатись лише з латинських літер і/або цифр, від 6 до 20 символів",
+				"Пароль повинен складатись з латинських літер і/або цифр, від 6 до 20 символів",
+				"Підтвердження паролю неправильне" },
+//			{ "//@#", "^$&*", "aaaa", "admin", "aa", "nnnnnnnn", "Ukraine",
+//				"Некоректне введення, лише літери", "Некоректне введення, лише літери", "Введіть коректну адресу",
+//				"Цей логін уже використовується",
+//				"Пароль повинен складатись з латинських літер і/або цифр, від 6 до 20 символів",
+//				"Підтвердження паролю неправильне" } 
+			};
+	}
+
+	@Test(dataProvider = "registrationInvalidData")
+	public void testCreateCownerWithInvalidData(String first_Name, String last_Name, String email, String login,
+			String password, String confirm_Password, String territorialCommunity, String firstNameErrors,
+			String lastNameErrors, String emailErrors, String loginErrors, String passwordErrors,
+			String confirmPasswordErrors) {
 		driver.findElement(By.partialLinkText("Зареєструвати співвласника")).click();
 		driver.findElement(By.id("firstName")).clear();
 		driver.findElement(By.id("firstName")).sendKeys(first_Name);
@@ -79,24 +78,42 @@ public class CreateCoowner {
 		driver.findElement(By.id("confirmPassword")).sendKeys(confirm_Password);
 		new Select(driver.findElement(By.id("territorial_Community"))).selectByVisibleText(territorialCommunity);
 		driver.findElement(By.id("submit")).click();
+
+		// verify that the manual registration page is still opened:
+		new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h4")));
+		assertEquals(driver.findElement(By.cssSelector("h4")).getText(), "Основна інформація");
 		
-		//verify that the non-confirmed page opened:
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h4")));
-		assertEquals(driver.findElement(By.cssSelector("h4")).getText(), By.partialLinkText("Непідтверджені співвласники"));
+		// verify that error messages are present on the page:
+		assertEquals(driver.findElement(By.id("firstName.errors")).getText(),firstNameErrors);
+		assertEquals(driver.findElement(By.id("lastName.errors")).getText(),lastNameErrors);
+		assertEquals(driver.findElement(By.id("email.errors")).getText(),emailErrors);
+		assertEquals(driver.findElement(By.id("login.errors")).getText(),loginErrors);
+		assertEquals(driver.findElement(By.id("password.errors")).getText(),passwordErrors);
+		assertEquals(driver.findElement(By.id("confirmPassword.errors")).getText(),confirmPasswordErrors);
+//		assertTrue(true, loginErrors);
 		
+		// verify that error messages have red color:
+		assertEquals(driver.findElement(By.id("firstName.errors")).getCssValue("color"), "rgb(255, 0, 0)");
+		assertEquals(driver.findElement(By.id("lastName.errors")).getCssValue("color"), "rgb(255, 0, 0)");
+		assertEquals(driver.findElement(By.id("email.errors")).getCssValue("color"), "rgb(255, 0, 0)");
+		assertEquals(driver.findElement(By.id("login.errors")).getCssValue("color"), "rgb(255, 0, 0)");
+		assertEquals(driver.findElement(By.id("password.errors")).getCssValue("color"), "rgb(255, 0, 0)");
+		assertEquals(driver.findElement(By.id("confirmPassword.errors")).getCssValue("color"), "rgb(255, 0, 0)");
+		
+		// go to 'non-confirmed' page :
+		driver.findElement(By.xpath("//a[@href=\"#\" and @data-toggle=\"dropdown\"]")).click();// 'Community' item
+		driver.findElement(By.partialLinkText("Непідтверджені")).click();
+
 		// Search co-owner in table
 		driver.findElement(By.id("inputIndex3")).click();
 		driver.findElement(By.id("inputIndex3")).clear();
 		driver.findElement(By.id("inputIndex3")).sendKeys(login);
 		driver.findElement(By.id("bth-search")).click();// Search button
-		//Verify that co-owner with given login is  present in the table
-		assertEquals(driver.findElement(By.cssSelector(".login")).getText(), login);
-		
+		assertFalse(false, login);// verify that co-owner with such login does not present in the table
+		assertTrue(true, "No data");// verify that text message is preent in the tab
 	}
-	/**
-	 * In this method author try create co-owner with invalid data
-	 */
-//	@Test(dataProvider = "mainRegistrationCorrectData")
+	
+//	@Test(dataProvider = "registrationInvalidData")
 	public void testClearRegistrationForm(String first_Name, String last_Name, String email, String login, String password,
 			String confirm_Password, String territorialCommunity) {
 		driver.findElement(By.partialLinkText("Зареєструвати співвласника")).click();
