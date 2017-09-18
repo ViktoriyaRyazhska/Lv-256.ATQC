bf373b2f310e18f25c8c07ee73aee417c14313d8
XTreme.ws <XTreme.ws@DESKTOP-F3FS9N9>
2017-09-18 13:54:45 +0300

    add new methods

--- test/framework/pages/DropdownHomePage.java
+++ test/framework/pages/DropdownHomePage.java
@@ -24,11 +24,16 @@ public abstract class DropdownHomePage extends TitleLocalFooter{
 		homeButton=driver.findElement(By.xpath("//*[@id=\"navigationbar\"]//following::a"));
 		userName = driver.findElement(By.cssSelector(".btn.btn-primary.btn-sm"));
 		downButton = driver.findElement(By.cssSelector(".btn.btn-primary.btn-sm.dropdown-toggle"));
+		
+	}
+	public void clickDownButton (WebDriver driver){	
+		getUserName().click();
+		getDownButton().click();
 		changePassword = driver.findElement(By.cssSelector(".change-password"));
 		resetPassword = driver.findElement(By.cssSelector(".reset-my-password"));
 		logout = driver.findElement(By.cssSelector("[href=\"/logout\"]"));
 	}
-
+	
 	public WebElement getHomeButton() {
 		return homeButton;
 	}
@@ -61,13 +66,13 @@ public abstract class DropdownHomePage extends TitleLocalFooter{
 		this.resetPassword = resetPassword;
 	}
 
-//	public WebElement getDownButton() {
-//		return downButton;
-//	}
-//
-//	public void setDownButton(WebElement downButton) {
-//		this.downButton = downButton;
-//	}
+	public WebElement getDownButton() {
+		return downButton;
+	}
+
+	public void setDownButton(WebElement downButton) {
+		this.downButton = downButton;
+	}
 
 	public WebElement getLogout() {
 		return logout;
