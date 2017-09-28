def2c5557951dd43793667c40b29ec4f5c4d0fc3
PETYAggg <petyaggg@gmail.com>
2017-09-28 22:11:52 +0300

    Implemented Static Factory builder for User(Login page) class. Added comments.

--- src/test/java/com/regres/testdata/User.java
+++ src/test/java/com/regres/testdata/User.java
@@ -1,18 +1,30 @@
 package com.regres.testdata;
 
-public class User {
+/**
+ * This class is made for the Login Page tests.
+ * User has two fields : login and password.
+ * Instances of this class are stored in UserContainer class.
+ * @author PETYAggg
+ *
+ */
+public class User implements Login,Password,BuildUser,IUser{
 
 	private String login;
 	private String password;
 
-	public User() {
+	private User() {
 	}
 
+	public static Login get() {
+		
+		return new User();
+	}
+	
 	public String getLogin() {
 		return login;
 	}
 
-	public User setLogin(String login) {
+	public Password setLogin(String login) {
 		this.login = login;
 		return this;
 	}
@@ -21,8 +33,13 @@ public class User {
 		return password;
 	}
 
-	public User setPassword(String password) {
+	public BuildUser setPassword(String password) {
 		this.password = password;
+		return this;
+	}
+	
+	public User build() {
+		
 		return this;
 	}
 
