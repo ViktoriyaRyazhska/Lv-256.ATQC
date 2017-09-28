e63646cfa399a48d6385790e0816beb3f5602f91
PETYAggg <petyaggg@gmail.com>
2017-09-28 22:13:05 +0300

    Added interface files.

---
+++ src/test/java/com/regres/testdata/IUser.java
@@ -0,0 +1,13 @@
+package com.regres.testdata;
+
+/**
+ * Implements User builder pattern.
+ * @author PETYAggg
+ *
+ */
+public interface IUser {
+	
+	String getLogin();
+	String getPassword();
+
+}
