baf3579ba1c43cd2810880c4c9c5d58c94b445e3
khrystynaTerletska <khrystynaterletska@gmail.com>
2017-10-24 09:57:40 +0300

    12

--- src/test/java/com/regres/dao/ResourceTypeDAO.java
+++ src/test/java/com/regres/dao/ResourceTypeDAO.java
@@ -2,11 +2,12 @@ package com.regres.dao;
 
 import java.util.List;
 
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import com.regres.application.HibernateSessionFactory;
+import  com.regres.entities.HibernateSessionFactory;
 import com.regres.entities.ResourceTypes;
 
 public class ResourceTypeDAO {
@@ -21,7 +22,7 @@ public class ResourceTypeDAO {
 		String typeName = "";
 		Session session = HibernateSessionFactory.currentSession();
 		Transaction tx = session.beginTransaction();
-		// По умолчанию entity name совпадает с именем класса.
+		// The default name of the organization is the same as the name of the class.
 		Query query = session.createQuery("from ResourceTypes where type_name = :subclassName");
 		query.setParameter("subclassName", enteredNameSubclass);
 		List<ResourceTypes> resourceTypes = query.list();
